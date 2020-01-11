84b8126c894a7416331ad68dace0b77ef09ed3da
perez6736 <perez6736@gmail.com>
2020-01-12 01:40:10 +0900

    fixed some syntax errors

--- csvreplace.py
+++ csvreplace.py
@@ -1,10 +1,12 @@
-cloneimport csv
+import csv
 import os
 
-full_path = os.path.expanduser('D:\Desktop/ramons thing/English/')
+fileName = 'DannyTest.csv'
+
+full_path = os.path.expanduser('D:/Desktop/TranslationSwap/')
 print (full_path)
 
-with open(full_path + "UI.tsv", encoding='utf-8') as tsvfile:
+with open(full_path + fileName, encoding='utf-8') as tsvfile:
     readCSV = csv.reader(tsvfile, delimiter='\t')
     for row in readCSV:
         print(row[1])
