0bad092ca7317155fb94024327f7a1d0dc100cb5
perez6736 <perez6736@gmail.com>
2020-01-13 14:00:25 +0900

    create a dictionary of Japanese - Korean

--- csvreplace.py
+++ csvreplace.py
@@ -2,12 +2,26 @@ import csv
 import os
 
 ## i would hope you can just swap the name of the files here to customize a bit 
-file1Name = 'DannyTest.csv'
-file2Name = 'DannyTestJap.csv'
+file1Name = 'DannyTest.csv' ## this file has the english we want to replace 
+file2Name = 'DannyTestJap.csv' ## this file has the Japanese we want to save 
 full_path = os.path.expanduser('D:/Desktop/TranslationSwap/')
 
 file1 = open(full_path + file1Name, encoding='utf-8')
 file2 = open(full_path + file2Name, encoding='utf-8')
+
+
+## read the csv file2
+readerFile2 = csv.reader(file2, delimiter='\t')
+Japanese2KoreanDict = {}
+for row in readerFile2:
+    print (row) ## this prints all rows 
+    Japanese2KoreanDict[row[2]] = row[1] ## dictionary is created here 
+print (Japanese2KoreanDict)
+
+
+## Now we need to loop through file1 (the one we want to replace englsih)
+ReaderFile1 = csv.reader(file1, delimiter='\t')
+
 
 
 ## open file 1 the actual game file 
